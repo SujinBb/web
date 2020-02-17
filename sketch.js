17d4fceb2b75a1bb596ed2c9f090ece95f66d15d
SujinBb <36348220+SujinBb@users.noreply.github.com>
2020-02-17 19:22:52 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,19 +7,19 @@ var sensativity = 0.005;
 
 
 function setup() {
-     createCanvas(1400,600);
      imageMode(CENTER);
      fingers = createVideo(['1920.mp4']);
      fingers.hide();
      song = loadSound('Untertitle.mp3');
+     song.loop();
+       createCanvas(1400,600);
+       background(255);
 
     
     
 }
 
 function draw() {
- 
-  background(255);
    fill(255,0,0);
    stroke(255,0,0)
    triangle(645, 120, 695, 90, 645, 60);
@@ -27,7 +27,7 @@ function draw() {
   translate(width/2,height/2);
   scale(zoom);
   image(fingers,0,0,320,320);
-  fingers.loop(); 
+
   
   
 
