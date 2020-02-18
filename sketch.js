3f502b4d1df63c68e2f9f24c3c516c0cc41e4fe4
SujinBb <36348220+SujinBb@users.noreply.github.com>
2020-02-18 09:25:21 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,46 +1,44 @@
+var playing = false;
 var vid;
-var song;
+var button;
 var zoom = 1.00;
 var zMin = 0.05;
 var zMax = 9.00;
 var sensativity = 0.005;
-
+var _self;
 
 function setup() {
-     createCanvas(1000,600);
-     imageMode(CENTER);
-     vid = createVideo( ['1920.mp4'], vidLoad);
-     vid.hide();
-     
-  }
-
-function vidLoad(){
-     
- vid.loop();
+  createCanvas(1000,700);
+  // specify multiple formats for different browsers
+  imageMode(CENTER);
+  vid = createVideo(['1920.mp4']);
+ vid.hide();
+  button = createButton('play');
+  button.mousePressed(toggleVid); 
+  button.position(500,10);
+  button.size(80,40);// attach button listener
+ button.style("background-color","#fff");
+ button.style("color","#f00");
+ button.style('font-size', '20px');
+ button = createButton("Sound");
+ button.mousePressed(LinkClik);
+ button.position(600,10);
+ button.size(80,40);
+ button.style("background-color","#fff");
+ button.style("color","#f00");
+ button.style('font-size', '20px');
  
+
 }
-
-function preload(){
- soundFormats('mp3', 'ogg');
- song = loadSound('Untertitle.mp3');
-   
-}
-
-
 function draw() {
  
   background(255);
-   fill(255,0,0);
-   stroke(255,0,0)
-   triangle(445, 120, 495, 90, 445, 60);
-   rect(530, 65, 50, 50);
   translate(width/2,height/2);
   scale(zoom);
-  image(vid,0,0,100,100);
+  image(vid,20,0,100,100);
   }
   
-
-function mouseWheel(event) {
+  function mouseWheel(event) {
   zoom += sensativity * event.delta;
   zoom = constrain(zoom, zMin, zMax);
   //uncomment to block page scrolling
@@ -48,14 +46,17 @@ function mouseWheel(event) {
 }
 
 // plays or pauses the video depending on current state
-function mousePressed() {
-  if (song.isPlaying()) {
-    // .isPlaying() returns a boolean
-    song.stop();
-   rect(720, 75, 50, 50);
+function toggleVid() {
+  if (playing) {
+    vid.pause();
+    button.html('play');
   } else {
-    song.play();
-    triangle(650, 127, 700, 100, 650, 70);
+    vid.loop();
+    button.html('pause');
   }
-  
+  playing = !playing;
+}
+
+function LinkClik(){
+ window.open("sound.html", _self);
 }
