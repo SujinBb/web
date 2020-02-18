b3704276988b049695a961fe157df0f4593528c3
SujinBb <36348220+SujinBb@users.noreply.github.com>
2020-02-18 10:03:43 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -15,7 +15,7 @@ function setup() {
  vid.hide();
   button = createButton("Sound");
  button.mousePressed(LinkClik);
- button.position(800,10);
+ button.position(700,10);
  button.size(80,40);
  button.style("background-color","#fff");
  button.style("color","#f00");
@@ -23,7 +23,7 @@ function setup() {
   
   button = createButton('play');
   button.mousePressed(toggleVid); 
-  button.position(700,10);
+  button.position(600,10);
   button.size(80,40);// attach button listener
  button.style("background-color","#fff");
  button.style("color","#f00");
@@ -36,7 +36,7 @@ function draw() {
   background(255);
   translate(width/2,height/2);
   scale(zoom);
-  image(vid,70,0,100,100);
+  image(vid,50,0,100,100);
   }
   
   function mouseWheel(event) {
